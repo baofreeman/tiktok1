cb3b32a364beaa705c028dc0ac0a6408021b4a21
baofreeman <freeman.dev10@gmail.com>
2023-01-06 00:08:25 +0700

    6/1

---
+++ src/component/Popper/Menu/index.js
@@ -0,0 +1,60 @@
+import classNames from 'classnames/bind';
+import styles from './Menu.module.scss';
+import Tippy from '@tippyjs/react/headless';
+import { Wrapper as PopperWrapper } from '~/component/Popper';
+import MenuItem from './MenuItem';
+import Header from './Header';
+import { useState } from 'react';
+
+const cx = classNames.bind(styles);
+
+function Menu({ children, items = [], onChange }) {
+    const [history, setHistory] = useState([{ data: items }]);
+    const current = history[history.length - 1];
+
+    console.log(current);
+    const renderMenu = () => {
+        return current.data.map((item, index) => {
+            const isParent = !!item.children;
+            return (
+                <MenuItem
+                    key={index}
+                    data={item}
+                    onClick={() => {
+                        if (isParent) {
+                            setHistory((prev) => [...prev, item.children]);
+                        } else {
+                            onChange(item);
+                        }
+                    }}
+                />
+            );
+        });
+    };
+
+    return (
+        <Tippy
+            delay={[0, 700]}
+            interactive
+            placement="bottom-end"
+            render={(attrs) => (
+                <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
+                    <PopperWrapper>
+                        {history.length > 1 && (
+                            <Header
+                                title={'Language'}
+                                onBack={() => setHistory((prev) => prev.slice(0, prev.length - 1))}
+                            />
+                        )}
+                        {renderMenu()}
+                    </PopperWrapper>
+                </div>
+            )}
+            onHide={() => setHistory((prev) => prev.slice(0, 1))}
+        >
+            {children}
+        </Tippy>
+    );
+}
+
+export default Menu;
